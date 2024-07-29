a883fce666076dc1733b0043c85a3c567b1db20a
Pratik <pratik1121999@gmail.com>
2024-07-29 17:20:40 +0530

    Intial commit

---
+++ src/main/java/com/enduser/config/KafkaConfig.java
@@ -0,0 +1,13 @@
+package com.enduser.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+@Configuration
+public class KafkaConfig {
+	
+	@KafkaListener(topics = AppConstant.LOCATION_UPDATE_TOPIC, groupId = AppConstant.GROUP_ID)
+	public void updatedLocation(String value) {
+		System.out.println(value);
+	}
+
+}
